0b40f7309ad22055ff4a652719d37e8410a18613
Teresa Barin <tbarin@gmail.com>
2017-05-19 23:42:54 +0000

    added devise gem

--- Gemfile
+++ Gemfile
@@ -65,4 +65,6 @@ gem 'will_paginate', '>= 3.1'
 
 gem 'font-awesome-rails'
 
+gem 'devise'
+
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  # added host for devise
+  config.action_mailer.default_url_options = { host: 'nomster-teresa-barin.herokuapp.com' }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
