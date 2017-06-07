4cb55537bf3226c61dc8a6ca5b758ab0a02b5e91
Teresa Barin <tbarin@gmail.com>
2017-06-07 22:07:08 +0000

    added send email to comment.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,17 +2,18 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   #added code for email delivery
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
+  #config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD']
-  }
+  #config.action_mailer.smtp_settings = {
+  #  address: "smtp.gmail.com",
+  #  port: 587,
+  #  domain: "example.com",
+  #  authentication: "plain",
+  #  enable_starttls_auto: true,
+  #  user_name: ENV['GMAIL_ADDRESS'],
+  #  password: ENV['GMAIL_PASSWORD']
+  #}
   # added code for devise
   config.action_mailer.default_url_options = { host: 'localhost:3030' }
   # In the development environment your application's code is reloaded on
